6864e1dc44f73771b29f0f7c3c1010ce0a58c297
Joaolucasoliveira <joao.l.alves@hotmail.com>
2019-04-05 12:15:14 -0400

    3 files modified

--- src/app/Viewer/viewer.component.ts
+++ src/app/Viewer/viewer.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ElementRef, OnInit, Input, SimpleChanges } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit, Input, SimpleChanges, HostListener } from '@angular/core';
 import { File } from './file'
 import { NavigationService } from './navigation.service'
 import { ResizeService } from './resize.service'
@@ -20,9 +20,12 @@ export class ViewerComponent implements OnInit {
 
   ngOnInit() {
     this.context = (<HTMLCanvasElement>this.canvasRef.nativeElement).getContext('2d');
+    let computedStyle = window.getComputedStyle((<HTMLDivElement>this.canvasWrapper.nativeElement), null);
 
-    (<HTMLCanvasElement>this.canvasRef.nativeElement).width = (<HTMLDivElement>this.canvasWrapper.nativeElement).clientWidth;
-    (<HTMLCanvasElement>this.canvasRef.nativeElement).height = (<HTMLDivElement>this.canvasWrapper.nativeElement).clientHeight;
+    console.log(computedStyle.width, computedStyle.height);
+
+    (<HTMLCanvasElement>this.canvasRef.nativeElement).width = parseInt(computedStyle.width);
+    (<HTMLCanvasElement>this.canvasRef.nativeElement).height = parseInt(computedStyle.height);
 
     this.loadFiles(this.files);
 
@@ -37,25 +40,41 @@ export class ViewerComponent implements OnInit {
     }
   }
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.updateCanvasSize();
+  }
+
+  updateCanvasSize() {
+    let computedStyle = window.getComputedStyle((<HTMLDivElement>this.canvasWrapper.nativeElement), null);
+
+    console.log(computedStyle.width, computedStyle.height);
+
+    (<HTMLCanvasElement>this.canvasRef.nativeElement).width = parseInt(computedStyle.width);
+    (<HTMLCanvasElement>this.canvasRef.nativeElement).height = parseInt(computedStyle.height);
+
+    this.drawOnCanvas(this.navigationService.pages[this.navigationService.selectedIndex]);
+  }
+
   drawOnCanvas(page) {
     if (page != null) {
       var ctx = this.context;
 
-      this.context.restore();
-      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+      // Store the current transformation matrix
+      ctx.restore();
 
-      //this.context.restore();
-      this.context.setTransform(1, 0, 0, 1, 0, 0);
+      // Use the identity matrix while clearing the canvas
+      ctx.setTransform(1, 0, 0, 1, 0, 0);
+      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
       var image = new Image();
       image.src = page.data;
       image.onload = () => {
 
-        let zoomValue = this.resizeService.calculateZoomFit((<HTMLCanvasElement>this.canvasRef.nativeElement).width, (<HTMLCanvasElement>this.canvasRef.nativeElement).height, image.width, image.height);
+        let zoomValue = this.resizeService.calculateZoomFit(ctx.canvas.width, ctx.canvas.height, image.width, image.height);
 
-        this.context.scale(zoomValue, zoomValue);
-        //(((<HTMLCanvasElement>this.canvasRef.nativeElement).width / 2))
-        this.context.drawImage(image, 0, 0);
+        ctx.setTransform(zoomValue, 0, 0, zoomValue, ctx.canvas.width / 2, ctx.canvas.height / 2);
+        ctx.drawImage(image, -image.width / 2, -image.height / 2);
 
         this.context.save();
       }
